29650801a1dc678175f8eab0cd976c296a97bcf3
anisnurhamsiyah <anisnurhamsiyah05@gmail.com>
2021-10-27 19:23:34 +0700

    first commit

---
+++ konstanta.php
@@ -0,0 +1,9 @@
+<?php  
+	define("hobi", "Membaca dan Menonton Film");
+	echo "Hallo guys, Welcome back to My Website Anis Nur Hamsiyah!! Hobby saya adalah";
+	echo hobi;
+	echo "<br>";
+	$jl=006;
+	echo "Saya tinggal di Banjarsawah Dusun arisan RT ";
+	echo $jl;
+?>
